be85e7b4a2a8e3b6d1edfd41631c80114eeacdce
xta <rexfeng@gmail.com>
2023-03-12 01:47:17 -0800

    task5: update layout and styling

--- Tasks/task5.swiftpm/ContentView.swift
+++ Tasks/task5.swiftpm/ContentView.swift
@@ -7,6 +7,10 @@ struct ContentView: View {
     @State private var inputName = ""
     @State private var inputSurname = ""
 
+    // temp names handle edge case where user selects a person in the list after they entered a new person
+    @State private var tempInputName = ""
+    @State private var tempInputSurname = ""
+
     @State private var people = [
         Person(name: "Andy", lastName: "Zoo"),
         Person(name: "Betty", lastName: "Yoo"),
@@ -16,49 +20,90 @@ struct ContentView: View {
     @State private var selection: Person?
 
     var body: some View {
-        HStack {
+        VStack {
+            Text("People")
+            List(displayList) {
+                SelectionCell(person: $0, selected: self.$selection)
+
+            }
+            .onChange(of: selection) { person in
+                if let person = person {
+                    tempInputName = inputName
+                    tempInputSurname = inputSurname
+
+                    inputName = person.name
+                    inputSurname = person.lastName
+                } else {
+                    inputName = tempInputName
+                    inputSurname = tempInputSurname
+                }
+            }
 
             VStack {
-                Text("People")
-
-                List(displayList) {
-                    SelectionCell(person: $0, selected: self.$selection)
+                if (selection != nil) {
+                    Text("Edit person")
+                } else {
+                    Text("Add person")
                 }
 
+                HStack {
+                    Text("Name")
+                        .foregroundColor(.secondary)
+                    TextField(
+                        "Enter Name",
+                        text: $inputName
+                    )
+                }
+                HStack {
+                    Text("Surname")
+                        .foregroundColor(.secondary)
+                    TextField(
+                        "Enter Surname",
+                        text: $inputSurname
+                    )
+                }
+                HStack {
+                    if (selection != nil) {
+                        Button("Update") {
+                            updatePerson()
+                        }
+                        .disabled(!selectionIsSelected)
+
+                        Spacer()
+                            .frame(width: 20)
+
+                        Button("Delete") {
+                            deletePerson()
+                        }
+                        .disabled(!selectionIsSelected)
+                    } else {
+                        Button("Create") {
+                            createPerson()
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .disabled(!inputIsEntered)
+                    }
+                }
+            }
+            .padding([.leading, .trailing], 50)
+            .padding([.top, .bottom], 10)
+
+            Divider()
+
+            VStack {
                 Text("Search for name")
                 TextField(
-                    "Name",
+                    "First name",
                     text: $prefixFilter
                 )
             }
-
-            VStack {
-                Text("Name")
-                TextField(
-                    "Enter Name",
-                    text: $inputName
-                )
-                Text("Surname")
-                TextField(
-                    "Enter Surname",
-                    text: $inputSurname
-                )
-
-                Button("Create") {
-                    createPerson()
-                }
-
-                Button("Update") {
-                    updatePerson()
-                }
-                .disabled(!selectionIsSelected)
-
-                Button("Delete") {
-                    deletePerson()
-                }
-                .disabled(!selectionIsSelected)
-            }
+            .padding([.leading, .trailing], 50)
+            .padding([.top, .bottom], 10)
         }
+    }
+
+    var inputIsEntered: Bool {
+        return inputName != "" && inputSurname != ""
     }
 
     var selectionIsSelected: Bool {
@@ -94,6 +139,8 @@ struct ContentView: View {
 
             inputName = ""
             inputSurname = ""
+
+            selection = nil
         }
     }
 
